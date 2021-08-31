b9ce5a6f3b381c3699d1047b4cdd980543ad4127
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-08-31 14:53:29 +0200

    1

---
+++ tasks.js
@@ -0,0 +1,17 @@
+function checkVal(val){
+    if (val !== undefined && val !== null)return true
+    else return false
+}
+var1 = [1,2,3]
+var2 = [100,2,1,10]
+var3 = []
+for (let i =0; i<var2.length;i++){
+    if (checkVal(var2[i])&&!var1.includes(var2[i])) {
+        var3.push(var2[i])
+    }
+    if (checkVal(var1[i])&&!var2.includes(var1[i])){
+        var3.push(var1[i])
+    }
+}
+console.log(var3)
+
