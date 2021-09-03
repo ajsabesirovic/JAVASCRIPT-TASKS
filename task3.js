679b74a21aba93b9e8a7ca43881d8172314f0ed4
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-03 12:10:20 +0200

    3

---
+++ task3.js
@@ -0,0 +1,18 @@
+// function digital_root(n) {
+//     n = n.toString()
+//     sum = 0
+//     for(let i = 0;i<n.length;i++){
+//       sum += Number(n[i])
+//     }
+//     if(sum>9){
+//       sum1 = 0
+//       sum = sum.toString()
+      
+//       for(let i = 0;i<sum.length;i++){sum1 += Number(sum[i])}
+//       if sum1
+//       return sum1 }
+//     else{return sum}
+//   }
+
+// console.log(digital_root(493193))
+
--- tasks.js
+++ tasks.js
@@ -17,24 +17,6 @@
 
 
 
-// function digital_root(n) {
-//     n = n.toString()
-//     sum = 0
-//     for(let i = 0;i<n.length;i++){
-//       sum += Number(n[i])
-//     }
-//     if(sum>9){
-//       sum1 = 0
-//       sum = sum.toString()
-      
-//       for(let i = 0;i<sum.length;i++){sum1 += Number(sum[i])}
-//       if sum1
-//       return sum1 }
-//     else{return sum}
-//   }
-
-// console.log(digital_root(493193))
-
 
 // function humanReadable(seconds) {
   
