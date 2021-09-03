0c24b4ebe539c5304b125fe7e84e342f0fbfce21
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-03 12:11:33 +0200

    4

---
+++ task4.js
@@ -0,0 +1,18 @@
+
+// function humanReadable(seconds) {
+  
+//     hh = Math.floor(seconds/3600)
+//     mm = Math.floor((seconds-hh*3600)/60)
+//     ss = Math.floor((seconds-(hh*3600)+(mm/60)))
+//     if(hh<10){hh='0'+hh}
+//     if(mm<10){mm='0'+mm}
+//     if(ss<10){ss='0'+ss}
+
+//     return `${hh}:${mm}:${ss}`
+// }
+
+// console.log(humanReadable(4))
+// function findSq(br){
+//     for(let i = 1;)
+// }
+
--- tasks.js
+++ tasks.js
@@ -17,26 +17,6 @@
 
 
 
-
-// function humanReadable(seconds) {
-  
-//     hh = Math.floor(seconds/3600)
-//     mm = Math.floor((seconds-hh*3600)/60)
-//     ss = Math.floor((seconds-(hh*3600)+(mm/60)))
-//     if(hh<10){hh='0'+hh}
-//     if(mm<10){mm='0'+mm}
-//     if(ss<10){ss='0'+ss}
-
-//     return `${hh}:${mm}:${ss}`
-// }
-
-// console.log(humanReadable(4))
-// function findSq(br){
-//     for(let i = 1;)
-// }
-
-
-
 lista = ['ryu','ken','chun-li','cammy','guile','sakura','sagat','juri']
 lista.pop()
 h = lista.length
