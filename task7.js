3099b645913848b84518b7c6535ba6c114b91da2
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-04 14:08:15 +0200

    7

---
+++ task7.js
@@ -0,0 +1,7 @@
+
+// array1 = [1,2,3]
+// array2 = [2,30,1]
+// nr = [43,34,64,43,7,76,1321,979,547657,6,7,7,34]
+// nr.sort(function(a,b){return a-b}).reverse()
+// nr = Array.from(new Set(nr))
+// console.log(nr[2])
