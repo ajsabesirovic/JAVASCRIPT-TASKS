e28ef43e4cf02db4441fd6bf536942e08f605520
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-04 14:06:54 +0200

    6

---
+++ task6.js
@@ -0,0 +1,12 @@
+// numbers = [25,25,30,10,15,35,10,25,30,10,15,20,10,40,50,60,70]
+// target = 50
+// for (let i = 0; i < numbers.length; i++){
+//     for(let o = 0; o < numbers.length; o++){
+//         if (numbers[i] + numbers[o] == target&&i!=o){
+//             console.log(numbers[i],numbers[o])
+//             numbers.splice(i,1)
+//             numbers.splice(o-1,1)
+//             break
+//         }
+//     }
+// }
