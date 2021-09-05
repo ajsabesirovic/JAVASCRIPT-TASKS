919ddd7e8088aeb302793ba62372cd351b1b04f7
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-05 11:54:44 +0200

    9

---
+++ task9.js
@@ -0,0 +1,14 @@
+// function kvadrat(niz){
+//     for(let i = 0; i < l; i++){
+//         niz[i] = niz[i] ** 2
+//     }
+//     return niz
+// }
+// function ispis(niz){
+//     for(let i of niz){
+//         console.log(i)
+//     }
+// }
+// mojNiz = kreiranjeNiza()
+// mojNiz = kvadrat([...mojNiz])
+// ispis(mojNiz)
