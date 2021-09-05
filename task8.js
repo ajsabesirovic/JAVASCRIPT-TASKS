252b2991658bc555febfa9467b3698e93d6e0e62
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-05 11:53:14 +0200

    8

---
+++ task8.js
@@ -0,0 +1,7 @@
+// function kreiranjeNiza(n){
+//     l = parseInt(prompt('unesite duzinu niza'))
+//     for(let i = 0; i < l; i++){
+//         n.push(parseInt(prompt('unesite el: ')))
+//     }
+//     return n;
+// }
