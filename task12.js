8be20d9eb050954e8030278b0c9f48c64bf7e486
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-07 14:13:22 +0200

    12

---
+++ task12.js
@@ -0,0 +1,21 @@
+// function mojaF(niz){
+//     niz.sort(function (a,b){
+//         return a-b;
+//     })
+//     sG = niz[niz.length -1]
+//     sS = niz[0]
+//     for (let i = niz.length - 1; i >= 0; i--){
+//         if (niz[i] < sG){
+//             sG =niz[i]
+//             break
+//         }
+//     }
+//     for (let i = 0;i  < niz.length; i++){
+//         if (niz[i] > sS){
+//             sS =niz[i]
+//             break
+//         }
+//     }
+//     console.log(sS,sG)
+// }
+// mojaF([1,2,3,4,5])
