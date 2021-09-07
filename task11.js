ded0b571a392a2dc5a612df30fd06cfc479624fb
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-07 14:11:28 +0200

    11

---
+++ task11.js
@@ -0,0 +1,9 @@
+// function mojaF(niz){
+//     niz.sort(function (a,b){
+//         return a-b;
+//     })
+//     niz = Array.from(new Set(niz))
+//     console.log(niz[1],niz[niz.length - 2])
+// }
+// mojaF([1,2,3,4,5])
+
