a9f7f2c77c46908672055a0e78c759fbd1def6ea
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-07 14:10:14 +0200

    10

---
+++ task10.js
@@ -0,0 +1,13 @@
+// function najduzaRec(s){
+//     ss = s.split(' ')
+//     najduza_rec = ''
+//     for(let i = 0; i < ss.length; i++){
+//         if (ss[i].length > najduza_rec.length){
+//             najduza_rec = ss[i]
+//         }
+//     }
+
+// return najduza_rec
+// }
+// console.log(najduzaRec('hello worlddd'))
+
