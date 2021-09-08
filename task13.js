2f1899335b512538f0d0d4d32fd00dae039cd01a
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-08 12:46:04 +0200

    13

---
+++ task13.js
@@ -0,0 +1,16 @@
+
+// function alphabetPosition(text){
+//     res = ''
+
+//     for(let i = 0; i < text.length; i++){
+//         letter = text[i].toLowerCase()
+//         letter_code = letter.charCodeAt(0)
+//         if (letter_code>=97 && letter_code<= 122 ){
+//             res += ' ' + (letter_code-96)
+
+//         }
+//     }
+//     return res.trim();
+// }
+// console.log(alphabetPosition(`The sunset sets at twelve o' clock.`))
+
