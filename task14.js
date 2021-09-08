d3c7574c1fe3ff849513abc756f781110252c152
Ajsa Besirovic <besirovicajsa@gmail.com>
2021-09-08 12:46:48 +0200

    14

---
+++ task14.js
@@ -0,0 +1,11 @@
+// function squareDigits(num){
+//     res = ''
+//     num = num.toString()
+//     for(let i = 0; i < num.length; i++){
+//         res += parseInt(num[i]) ** 2
+//     }
+//     return res
+// }
+// console.log(squareDigits(246))
+
+
